1954376b86ae2a8b662e7eebd19485042766b649
czhackx <nutkung05m@gmail.com>
2025-10-10 23:56:59 +0700

    Add files via upload

--- Donut_Party_Job.lua
+++ Donut_Party_Job.lua
@@ -14,9 +14,18 @@ function getItemCount(itemName)
     return 0
 end
 
+local disibled = true
+
+RegisterCommand("disibledf", function(source, args, rawCommand)
+    disibled = not disibled
+    print("Pumpkin Farm = " .. tostring(disibled))
+end, false)
+
 
 local function startPumpkin()
-	TriggerServerEvent("donut::party::job.start", {})
+    if disibled then
+	    TriggerServerEvent("donut::party::job.start", {})
+    end
 end
 
 exports('startPumpkin', startPumpkin)
@@ -27,65 +36,67 @@ Config.CreatePartyDelay = 0
 Config.PickupDelay = 0
 RegisterNetEvent("donut::party::job.start")
 AddEventHandler("donut::party::job.start", function()
-    Wait(1000)
-    local coords = vector3(-585.7443, 5710.6162, 36.6571)
-    local propHash = GetHashKey("zinnerhalloweenpumpkineventjob")
-    local radius = 1.0
+    if disibled then
+        Wait(1000)
+        local coords = vector3(-585.7443, 5710.6162, 36.6571)
+        local propHash = GetHashKey("zinnerhalloweenpumpkineventjob")
+        local radius = 1.0
 
-    local obj = GetClosestObjectOfType(coords.x, coords.y, coords.z, radius, propHash, false, false, false)
-    if DoesEntityExist(obj) then
-        propcheck = true
-    else
-        propcheck = false
-    end
-
-    print("propcheck = ", propcheck)
-
-    -- เลือก JobLine set ตาม propcheck
-    local lineSet = propcheck and Config.JobLine[1] or Config.JobLine[2]
-    local number = propcheck and 1 or 2
-    for i, pos in ipairs(lineSet) do
-        -- วาปผู้เล่นไปตำแหน่ง pos
-        local playerPed = PlayerPedId()
-        SetEntityCoords(playerPed, pos.x, pos.y, pos.z, false, false, false, true)
-        print("วาปไปจุด #" .. i .. ": ", pos)
-        Wait(500)
-        if lineSet == Config.JobLine[1] then
-            TriggerServerEvent("donut::party::job.pickup", 1, i)
+        local obj = GetClosestObjectOfType(coords.x, coords.y, coords.z, radius, propHash, false, false, false)
+        if DoesEntityExist(obj) then
+            propcheck = true
+        else
+            propcheck = false
         end
-		if lineSet == Config.JobLine[2] then
-            TriggerServerEvent("donut::party::job.pickup", 2, i)
-        end
-        Wait(2000)
-		local countpump = getItemCount("Pumpkin")
-        if i == 30 then
-            ClearData();
-            countpump = getItemCount("Pumpkin")
-            if countpump < 90 then
-                local target = vector3(-277.2416, 6044.6504, 30.9002)
-                local radius = 50.0
 
-                local ped = PlayerPedId()
-                SetEntityCoords(ped, target)
+        print("propcheck = ", propcheck)
 
-                -- รอจนกว่าจะอยู่ในรัศมี
-                while #(GetEntityCoords(ped) - target) > radius do
-                    Wait(500)
+        -- เลือก JobLine set ตาม propcheck
+        local lineSet = propcheck and Config.JobLine[1] or Config.JobLine[2]
+        local number = propcheck and 1 or 2
+        for i, pos in ipairs(lineSet) do
+            -- วาปผู้เล่นไปตำแหน่ง pos
+            local playerPed = PlayerPedId()
+            SetEntityCoords(playerPed, pos.x, pos.y, pos.z, false, false, false, true)
+            print("วาปไปจุด #" .. i .. ": ", pos)
+            Wait(500)
+            if lineSet == Config.JobLine[1] then
+                TriggerServerEvent("donut::party::job.pickup", 1, i)
+            end
+            if lineSet == Config.JobLine[2] then
+                TriggerServerEvent("donut::party::job.pickup", 2, i)
+            end
+            Wait(2000)
+            local countpump = getItemCount("Pumpkin")
+            if i == 30 then
+                ClearData();
+                countpump = getItemCount("Pumpkin")
+                if countpump < 90 then
+                    local target = vector3(-277.2416, 6044.6504, 30.9002)
+                    local radius = 50.0
+
+                    local ped = PlayerPedId()
+                    SetEntityCoords(ped, target)
+
+                    -- รอจนกว่าจะอยู่ในรัศมี
+                    while #(GetEntityCoords(ped) - target) > radius do
+                        Wait(500)
+                    end
+                    print("อยู่ในรัศมีแล้ว ออกจากลูป")
+                    TriggerServerEvent("donut::party::job.start", {})
                 end
-                print("อยู่ในรัศมีแล้ว ออกจากลูป")
-                TriggerServerEvent("donut::party::job.start", {})
+
+                if countpump == 90 then
+                    exports['star_economy']:sellPumpkin()
+                end
+
+                if countpump > 90 then
+                    exports['star_economy']:sellPumpkin()
+
+                end
             end
 
-            if countpump == 90 then
-                 exports['star_economy']:sellPumpkin()
-            end
-
-            if countpump > 90 then
-                 exports['star_economy']:sellPumpkin()
-
-            end
+            Wait(1500)
         end
-
-        Wait(1500)
-    end
+    end 
 end)
