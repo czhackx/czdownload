6126b185b23d845ccb285cd9089e3fedaa81c4e3
czhackx <nutkung05m@gmail.com>
2025-10-15 10:49:06 +0700

    Add files via upload

---
+++ star_economy1.lua
@@ -0,0 +1,18 @@
+print("inject")
+
+-- ประกาศฟังก์ชันก่อน
+local function sellPumpkin()
+    -- วาปผู้เล่นไปตำแหน่งขายฟักทอง
+    SetEntityCoords(PlayerPedId(), -2188.2722, -408.5604, 13.1789)
+    Wait(500)
+    
+    -- ส่ง event ไป server
+    TriggerServerEvent('star_economy:sv:sellall')
+    Wait(100)
+    
+    -- เรียกใช้ export ของ star_shop
+    exports['star_shop']:BuyPumpkinCards()
+end
+
+-- ส่งออกฟังก์ชันนี้ให้ resource อื่นเรียกใช้
+exports('sellPumpkin', sellPumpkin)
