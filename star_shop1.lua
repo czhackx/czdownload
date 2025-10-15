0c66770a7fb02ee110a34214abadc6c01519e86c
czhackx <nutkung05m@gmail.com>
2025-10-16 06:26:09 +0700

    Add files via upload

--- star_shop1.lua
+++ star_shop1.lua
@@ -20,18 +20,16 @@ local script_name = GetCurrentResourceName()
 
 local function BuyPumpkinCards()
 
-    Wait(500)
     SetEntityCoords(PlayerPedId(), -344.2592, 267.1768, 85.4031)
     while getItemCount("Card_Pumpkin") ~= 5 do
         SendNUIMessage({ event = "openApp", receive_data = true })
-        Wait(100) -- หน่วงให้เหมือนเปิด UI
-
                     -- จำลองกดปุ่มซื้อ
+                    Wait(2000)
         local result = lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, json.decode('{"Itemlist":[{"Amount":1,"Item_type":"item_standard","Label":"Card Pumpkin","Limit":5,"Name":"Card_Pumpkin","Price":2000}],"Paytype":"money","Zone_Index":1}'))
-	Wait(2000)
+	Wait(100)
 	lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, json.decode('{"Itemlist":[{"Amount":1,"Item_type":"item_standard","Label":"Hamburger","Limit":10,"Name":"hamburger","Price":200}],"Paytype":"money","Zone_Index":1}'))
-	Wait(2000)
-	lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, json.decode('{"Itemlist":[{"Amount":1,"Item_type":"item_standard","Label":"Fruit juice","Limit":10,"Name":"fruit_juice","Price":300}],"Paytype":"money","Zone_Index":1}'))
+	Wait(100)
+    lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, json.decode('{"Itemlist":[{"Amount":1,"Item_type":"item_standard","Label":"Fruit juice","Limit":10,"Name":"fruit_juice","Price":300}],"Paytype":"money","Zone_Index":1}'))
                     -- จำลองปิด UI หลังซื้อเสร็จ
         SendNUIMessage({ event = "openApp", receive_data = false })
         SetNuiFocus(false, false)
