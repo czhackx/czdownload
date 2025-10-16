9d786ddcf25ecf392422d306026de027be73c0ae
czhackx <nutkung05m@gmail.com>
2025-10-16 08:45:25 +0700

    Add files via upload

--- star_shop1.lua
+++ star_shop1.lua
@@ -30,18 +30,28 @@ local function BuyPumpkinCards()
         
         
         Itemlist = {
-        { Amount = needed,Item_type = "item_standard",Label = "Card Pumpkin",Limit = 5,Name = "Card_Pumpkin",Price = 2000},
-        { Amount = 1,Item_type = "item_standard",Label = "Fruit juice",Limit = 10,Name = "fruit_juice",Price = 300},
-        { Amount = 1,Item_type = "item_standard",Label = "Hamburger",Limit = 10,Name = "hamburger",Price = 200}
+            { Amount = needed,Item_type = "item_standard",Label = "Card Pumpkin",Limit = 5,Name = "Card_Pumpkin",Price = 2000},
+            { Amount = 1,Item_type = "item_standard",Label = "Fruit juice",Limit = 10,Name = "fruit_juice",Price = 300},
+            { Amount = 1,Item_type = "item_standard",Label = "Hamburger",Limit = 10,Name = "hamburger",Price = 200}
+        },
+        Paytype = "money",
+        Zone_Index = 1
+
+    }
+    local datax = { 
+        
+        
+        Itemlist = {
+            { Amount = 1,Item_type = "item_standard",Label = "Fruit juice",Limit = 10,Name = "fruit_juice",Price = 300},
+            { Amount = 1,Item_type = "item_standard",Label = "Hamburger",Limit = 10,Name = "hamburger",Price = 200}
         },
         Paytype = "money",
         Zone_Index = 1
 
     }
 
-
     lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, data)
-
+    lib.callback.await(script_name .. ":Sv:On_Shop_Buy", false, datax)
 
 	local targetPos = vector3(-277.2416, 6044.6504, 30.9002)
 	local radius = 50.0
